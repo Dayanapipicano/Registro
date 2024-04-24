16e13c36b6da44231d0f468ad2b58e79aae729b2
dayana <pipicanodayana2005@gmail.com>
2024-04-24 14:34:35 -0500

    Curso

--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from apps.usuarios.models import Estudiante
+from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from apps.usuarios.models import Curso
 
-# Create your views here.
+
+#CREAR CURSO
+
+def crear_curso(request):
+    if request.method == 'POST':
+        nombre = request.POST.get('nombre')
+        
+        curso =  Curso.objects.create(
+            nombre = nombre
+        )
+        return redirect('listar_curso')
+    else:
+        return render(request, 'curso/crear_curso.html')
+
+
+def listar_curso(request):
+    curso = Curso.objects.all() 
+    return render(request,'curso/listar_curso.html', {'curso':curso})
--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from apps.usuarios.views import crear_curso,listar_curso
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    #CURSO
+    path('crear_curso', crear_curso, name='crear_curso'),
+    path('listar_curso', listar_curso, name='listar_curso'),
 ]
